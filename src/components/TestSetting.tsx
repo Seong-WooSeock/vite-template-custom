44f44e7990d8200fcd91b730ca89a0ab5fa07d5d
wooseock-seong <wooseock-seong@nhn-commerce.com>
2022-08-20 13:17:51 +0900

    test husky and lint prev commit

---
+++ src/components/TestSetting.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TestSetting() {
+    const hi = alert('hi');
+    return (
+        <>
+            <h2>hello world</h2>
+        </>
+    )
+}
