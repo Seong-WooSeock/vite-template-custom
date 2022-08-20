eea517271ae20c28ef117218f5b150704d07868c
wooseock-seong <wooseock-seong@nhn-commerce.com>
2022-08-20 14:36:45 +0900

    todo

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{
-      find: '@',
-      replacement: path.resolve(__dirname, 'src')
-    }],
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
-})
+});
+//내일 비트 세팅 ㄱ
